efefbecc3ededb433d95694544535970e12ebb0c
Maduranga <madurangakodithuwakku@gmail.com>
2019-07-21 12:15:46 +0530

    Scroll navigation added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 import AppHeader from "./components/AppHeader";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
 import { Provider } from "react-redux";
 import Store from "./redux/Store";
+import Hero from "./components/Hero";
+import Portfolio from "./components/Portfolio";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
 
+const scrollToRef = ref =>
+  window.scrollTo({ top: ref.current.offsetTop, behavior: "smooth" });
 const theme = createMuiTheme({
   typography: {
     fontFamily: ["Lato"].join(",")
   }
 });
 function App() {
+  const myRef = useRef(null);
+  const myRefAbout = useRef(null);
+  const myRefContact = useRef(null);
   return (
     <Provider store={Store}>
       <ThemeProvider theme={theme}>
         <div className="App">
-          <AppHeader />
+          <AppHeader
+            onChangePortfolio={() => scrollToRef(myRef)}
+            onChangeAbout={() => scrollToRef(myRefAbout)}
+            onChangeContact={() => scrollToRef(myRefContact)}
+          />
+          <Hero />
+          <div ref={myRef}>
+            <Portfolio />
+          </div>
+          <div ref={myRefAbout}>
+            <AboutMe />
+          </div>
+          <div ref={myRefContact}>
+            <ContactMe />
+          </div>
         </div>
       </ThemeProvider>
     </Provider>
